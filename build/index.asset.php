61b5ec0a320d507d08b311572dd5e3ff6b6a0a5f
Ali Alaa <hello@alialaa.com>
2022-01-15 18:11:24 +0200

    Metadata

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'f5e6b814e1de75768b5b381e22df3b7f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '1ff2f15c731f8160188b3a0e579d1a9f');
